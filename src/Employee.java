24c86005d755ae207b86edc2aece6d53186445be
olmnlo <hosamag54@gmail.com>
2025-07-06 14:31:11 +0300

    Employee class

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,56 @@
 public class Employee {
+    private String id;
+    private String name;
+    private int salary;
 
+    public Employee() {
+
+    }
+
+    public Employee(String id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAnnualSalary(int annual){
+        return this.salary*annual;
+    }
+
+    public int raisedSalary(int percent){
+        return this.salary*percent;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
